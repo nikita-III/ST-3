f868ce77b1d25ae74f30fa1fb4c3e977fb125691
nikita-III <143026871+nikita-III@users.noreply.github.com>
2026-06-02 18:06:16 +0300

    Update TimedDoor.h2

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -6,18 +6,18 @@
 #include <stdexcept>
 
 class DoorTimeoutException : public std::runtime_error {
-public:
+ public:
     DoorTimeoutException() : std::runtime_error("Door open too long") {}
 };
 
 class TimerClient {
-public:
+ public:
     virtual void Timeout() = 0;
     virtual ~TimerClient() = default;
 };
 
 class Door {
-public:
+ public:
     virtual void lock() = 0;
     virtual void unlock() = 0;
     virtual bool isDoorOpened() = 0;
@@ -27,20 +27,20 @@ public:
 class TimedDoor;
 
 class DoorTimerAdapter : public TimerClient {
-private:
+ private:
     TimedDoor& door;
-public:
+ public:
     explicit DoorTimerAdapter(TimedDoor& d);
     void Timeout() override;
 };
 
 class TimedDoor : public Door {
-private:
+ private:
     DoorTimerAdapter* adapter;
     int iTimeout;
     bool isOpened;
 
-public:
+ public:
     explicit TimedDoor(int timeout);
     ~TimedDoor();
 
@@ -54,7 +54,7 @@ public:
 };
 
 class Timer {
-public:
+ public:
     virtual ~Timer() = default;
     virtual void tregister(int timeout, TimerClient* client);
 };
