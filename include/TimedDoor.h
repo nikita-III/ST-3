46da35a112d14b296063326e179a0be11948e1c8
nikita-III <143026871+nikita-III@users.noreply.github.com>
2026-06-02 18:02:08 +0300

    Update TimedDoor.h1

--- include/TimedDoor.h
+++ include/TimedDoor.h
@@ -3,50 +3,60 @@
 #ifndef INCLUDE_TIMEDDOOR_H_
 #define INCLUDE_TIMEDDOOR_H_
 
-class DoorTimerAdapter;
-class Timer;
-class Door;
-class TimedDoor;
+#include <stdexcept>
+
+class DoorTimeoutException : public std::runtime_error {
+public:
+    DoorTimeoutException() : std::runtime_error("Door open too long") {}
+};
 
 class TimerClient {
- public:
-  virtual void Timeout() = 0;
+public:
+    virtual void Timeout() = 0;
+    virtual ~TimerClient() = default;
 };
 
 class Door {
- public:
-  virtual void lock() = 0;
-  virtual void unlock() = 0;
-  virtual bool isDoorOpened() = 0;
+public:
+    virtual void lock() = 0;
+    virtual void unlock() = 0;
+    virtual bool isDoorOpened() = 0;
+    virtual ~Door() = default;
 };
 
+class TimedDoor;
+
 class DoorTimerAdapter : public TimerClient {
- private:
-  TimedDoor& door;
- public:
-  explicit DoorTimerAdapter(TimedDoor&);
-  void Timeout();
+private:
+    TimedDoor& door;
+public:
+    explicit DoorTimerAdapter(TimedDoor& d);
+    void Timeout() override;
 };
 
 class TimedDoor : public Door {
- private:
-  DoorTimerAdapter * adapter;
-  int iTimeout;
-  bool isOpened;
- public:
-  explicit TimedDoor(int);
-  bool isDoorOpened();
-  void unlock();
-  void lock();
-  int  getTimeOut() const;
-  void throwState();
+private:
+    DoorTimerAdapter* adapter;
+    int iTimeout;
+    bool isOpened;
+
+public:
+    explicit TimedDoor(int timeout);
+    ~TimedDoor();
+
+    bool isDoorOpened() override;
+    void unlock() override;
+    void lock() override;
+    int getTimeOut() const;
+    void throwState();
+
+    DoorTimerAdapter* getAdapter() const;
 };
 
 class Timer {
-  TimerClient *client;
-  void sleep(int);
- public:
-  void tregister(int, TimerClient*);
+public:
+    virtual ~Timer() = default;
+    virtual void tregister(int timeout, TimerClient* client);
 };
 
 #endif  // INCLUDE_TIMEDDOOR_H_
